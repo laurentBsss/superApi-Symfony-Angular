0105033f22c925a58f5dc942a0c381d66c38fb1e
LAURENT BASSES <LAURENT BASSES>
2016-10-12 17:48:17 +0200

    add nelmio api doc bundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new LBSS\MyAngularApiBundle\LBSSMyAngularApiBundle(),
+            new Nelmio\ApiDocBundle\NelmioApiDocBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
