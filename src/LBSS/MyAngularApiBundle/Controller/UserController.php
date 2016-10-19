21ba746455106b2fb2f3610ca01d82fc934625bb
LAURENT BASSES <LAURENT BASSES>
2016-10-19 20:11:31 +0200

    add form type for post method and add method remove

--- src/LBSS/MyAngularApiBundle/Controller/UserController.php
+++ src/LBSS/MyAngularApiBundle/Controller/UserController.php
@@ -13,6 +13,7 @@ use FOS\RestBundle\Controller\Annotations as Rest;
 /*use FOS\RestBundle\View\ViewHandler;
 use FOS\RestBundle\View\View;
 */
+use LBSS\MyAngularApiBundle\Form\UserType;
 
 
 class UserController extends Controller
@@ -24,17 +25,6 @@ class UserController extends Controller
     public function getUsersAction(Request $request)
     {   
     	$users = $this->getDoctrine()->getRepository('LBSSMyAngularApiBundle:User')->findAll();
-    	
-    	/*var_dump($users);die;*/
-        // Récupération du view handler
-       /* $viewHandler = $this->get('fos_rest.view_handler');
-
-        // Création d'une vue FOSRestBundle
-        $view = View::create($users);
-        $view->setFormat('json');
-
-        // Gestion de la réponse
-        return $viewHandler->handle($view);*/
 
         return $users;
 
@@ -48,19 +38,56 @@ class UserController extends Controller
     {   
     	$user = $this->getDoctrine()->getRepository('LBSSMyAngularApiBundle:User')->find($user);
 
-        /*// Récupération du view handler
-        $viewHandler = $this->get('fos_rest.view_handler');
-
-        // Création d'une vue FOSRestBundle
-        $view = View::create($user);
-        $view->setFormat('json');
-
-        // Gestion de la réponse
-        return $viewHandler->handle($view);*/
-        
          return $user;
 
     }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_CREATED)
+     * @Rest\Post("/users")
+     */
+    public function postUsersAction(Request $request)
+    { 
+        /*$userName = "Da vinci";
+        $email = "blabla@gmail.com" ;*/
+
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->submit($request->request->all());
+        /*$user->setUsername($request->get('username'))
+            ->setEmail($request->get('email'));*/
+       /* $user->setUsername($userName)
+            ->setEmail($email);*/
+
+         if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+            return $user;
+        } else {
+            return $form;
+        }
+
+    }
+
+    /**
+     * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
+     * @Rest\Delete("/users/{id}")
+     */
+    public function removeUserAction(User $user)
+    {
+        $user = $this->getDoctrine()->getRepository('LBSSMyAngularApiBundle:User')->find($user);
+        $em = $this->getDoctrine()->getManager();
+
+        if ($place) {
+            $em->remove($user);
+            $em->flush();
+        }
+
+    }
+
+
 
    
 }
