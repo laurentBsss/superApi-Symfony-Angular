f0bbb8e243bdb51434deb1b42179050e2e5b6cad
LAURENT BASSES <LAURENT BASSES>
2016-10-19 20:04:58 +0200

    add user formtype

---
+++ src/LBSS/MyAngularApiBundle/Form/UserType.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace LBSS\MyAngularApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class UserType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('username', TextType::class)
+            ->add('email', TextType::class)
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'LBSS\MyAngularApiBundle\Entity\User',
+            'csrf_protection' => false
+        ));
+    }
+}
