21223a58ca1ad0b64e1f31db4754d30a7692f829
laurentBsss <danone97200@gmail.com>
2016-11-02 11:54:01 +0100

    test on tracking controller

--- src/LBSS/MyAngularApiBundle/Controller/TrackingController.php
+++ src/LBSS/MyAngularApiBundle/Controller/TrackingController.php
@@ -8,14 +8,11 @@ use LBSS\MyAngularApiBundle\Entity\Tracking;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use LBSS\MyAngularApiBundle\Entity\Tracking;
 
 use FOS\RestBundle\Controller\Annotations as Rest;
 
 /*use LBSS\MyAngularApiBundle\Form\TrackingType;*/
 use GuzzleHttp\Client;
-
-use Symfony\Component\HttpFoundation\Response;
 
 class TrackingController extends Controller
 {
@@ -23,10 +20,15 @@ class TrackingController extends Controller
      * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
      * @Rest\Get("/tracking/{id}")
      */  
-    public function getTrackingAction(Request $request)
-    {   
-    	
-        return $tracking;
+    public function getTrackingAction($id)
+    {  /*var_dump($id);die;*/
+       $datatest = 'KU998077933FR';
+       $client = new Client( ['verify' => false ,'headers' => ['Accept'     => 'application/json','X-Okapi-Key' => 'WhxruvdlPdnFLcWJT9X7xZyhErjUqYa0Va7c/rxMfTepZ02LnPyKPrjrkV4V3joi']]);
+       $response = $client->get('https://api.laposte.fr/suivi/v1/'. $id);
+       $contents = $response->getBody()->getContents();
+       $response = new Response($contents);
+       $response->headers->set('Content-Type', 'application/json');
+       return $response;
 
     }
     
@@ -34,7 +36,7 @@ class TrackingController extends Controller
      * @Rest\View(statusCode=Response::HTTP_NO_CONTENT)
      * @Rest\Get("/tracking/test/{id}")
      */
-    public function getTrackingAction(Tracking $tracking)
+    public function getTrackingsAction(Tracking $tracking)
     {   
     	$user = $this->getDoctrine()->getRepository('LBSSMyAngularApiBundle:Tracking')->findAll();
 
